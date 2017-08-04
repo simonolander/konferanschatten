4ce54dc767ec1fc83c51ecf9c7fb85203d41ff01
Simon Olander <simon.olander@r2m.se>
2017-08-04 22:57:25 +0200

    break word

--- src/client/app/components/ChatMessage.jsx
+++ src/client/app/components/ChatMessage.jsx
@@ -2,18 +2,27 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Media } from 'react-bootstrap'
 
+const style = {
+  username: {
+    color: 'green'
+  },
+  text: {
+    wordBreak: 'break-word'
+  }
+}
+
 class ChatMessage extends Component {
   render () {
     return (
-      <Media>
+      <div>
         {/*<Media.Left>*/}
           {/*<img width={64} height={64} src="public/images/goat.png" alt="Image"/>*/}
         {/*</Media.Left>*/}
-        <Media.Body>
-          <Media.Heading>{this.props.username}</Media.Heading>
-          <p>{this.props.text}</p>
-        </Media.Body>
-      </Media>
+        {/*<Media.Body>*/}
+          <Media.Heading><h5 style={style.username}>{this.props.username}<small><i> liten text</i></small></h5></Media.Heading>
+          <p style={style.text}>{this.props.text}</p>
+        {/*</Media.Body>*/}
+      </div>
     )
   }
 }
